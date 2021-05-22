f2135ba72d4194fe81f4bfb37cb42d53d1cb3b8d
Douglas Mendes <douglasaleixomendes@gmail.com>
2021-05-22 15:49:04 -0300

    update log running

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.SERVER_PORT || 3040);
   new Logger(bootstrap.name).log(
-    `Api gateway running in port ${process.env.SERVER_PORT || 3040}`,
+    `Api Community running in port ${process.env.SERVER_PORT || 3040}`,
   );
 }
 bootstrap();
